8959ae54b47863e14a3b7e0f3bb65f6f9ea3eeb5
Areejshaikh <160318564+Areejshaikh@users.noreply.github.com>
2024-03-20 23:01:40 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+const user_output = await inquirer.prompt([
+    {
+        message: "Enter your first Number",
+        type: "number",
+        name: "firstNumber",
+    },
+    {
+        message: "Enter your second Number",
+        type: "number",
+        name: "secondNumber",
+    },
+    {
+        message: "Seclect any one option",
+        type: "list",
+        name: "oprators",
+        choices: ["addition", "subtraction", "multiplication", "division"],
+    },
+]);
+if (user_output.oprators === "addition") {
+    console.log(chalk.red `your output is:${user_output.firstNumber + user_output.secondNumber}`);
+}
+else if (user_output.oprators === "subtraction") {
+    console.log(chalk.yellowBright `your output is:${user_output.firstNumber - user_output.secondNumber}`);
+}
+else if (user_output.oprators === "multiplication") {
+    console.log(chalk.yellowBright `your output is:${user_output.firstNumber * user_output.secondNumber}`);
+}
+else if (user_output.oprators === "division") {
+    console.log(chalk.cyanBright `your output is:${user_output.firstNumber / user_output.secondNumber}`);
+}
+else {
+    console.log("Please Enter your valid number");
+}
